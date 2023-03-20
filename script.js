d43f4cda74ceef5c12a7a585ed1cf0aa3fb88993
Pedro Omena <108802516+Omeninha@users.noreply.github.com>
2023-03-20 12:35:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+const passwordRegex = /^[A-Za-z0-9]\w{8,}$/;
+const loginBtn = document.getElementById("login");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  email.style.border = "none";
+  password.style.border = "none";
+
+  if (!emailRegex.test(email.value) && !passwordRegex.test(password.value)) {
+    email.style.border = "2px solid red";
+    password.style.border = "2px solid red";
+  } else if (
+    emailRegex.test(email.value) &&
+    !passwordRegex.test(password.value)
+  ) {
+    password.style.border = "2px solid red";
+  } else if (
+    !emailRegex.test(email.value) &&
+    passwordRegex.test(password.value)
+  ) {
+    email.style.border = "2px solid red";
+  } else {
+    email.style.border = "2px solid rgb(0, 156, 60)";
+    password.style.border = "2px solid rgb(0, 156, 60)";
+  }
+});
 
